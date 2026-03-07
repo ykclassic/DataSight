caaedc7ddd1cebe0cdcd441153e4ba1557f9ad60
TechSolute <pharshiesammy@gmail.com>
2026-03-07 18:11:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ import dash
 from layout import layout
 from callbacks import register_callbacks
 
+# Initialize Dash
 app = dash.Dash(__name__)
+
+# Expose Flask server for Gunicorn
 server = app.server
 
+# Layout
 app.layout = layout
 
+# Register callbacks
 register_callbacks(app)
 
 if __name__ == "__main__":
