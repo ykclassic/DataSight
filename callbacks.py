62badd7b7a9135f1183fd824c39b3a0c779bbd95
TechSolute <pharshiesammy@gmail.com>
2026-03-07 17:53:01 +0100

    Update callbacks.py

--- callbacks.py
+++ callbacks.py
@@ -4,7 +4,9 @@ import pandas as pd
 from core.file_loader import parse_uploaded_file
 from core.schema_analyzer import analyze_schema
 from core.chart_builder import build_chart
-
+from core.anomaly_engine import detect_anomalies
+from core.insight_engine import generate_insights
+from core.recommendation_engine import generate_recommendations
 
 def register_callbacks(app):
 
